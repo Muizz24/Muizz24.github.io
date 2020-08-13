7fcf71e20efba9a3f11633e05735ccec7bf5695a
Muizz Ahmed <muizz.ahmed323@gmail.com>
2020-08-13 18:28:33 -0400

    Updated Projects Tabs
    Updated Projects Tabs to have more detail in animations and will add more to it later on.

---
+++ index.js
@@ -0,0 +1,31 @@
+$("#SoftwareExpPanel").hover(
+    function() {
+       $('.softwareDevCard').collapse('show');
+     }, function() {
+       $('.softwareDevCard').collapse('hide');
+     }
+);
+
+$("#GameDevExpPanel").hover(
+    function() {
+       $('.gameDevCard').collapse('show');
+     }, function() {
+       $('.gameDevCard').collapse('hide');
+     }
+);
+
+$("#WebDevExpPanel").hover(
+    function() {
+       $('.webDevCard').collapse('show');
+     }, function() {
+       $('.webDevCard').collapse('hide');
+     }
+);
+
+$("#DatabaseExpPanel").hover(
+    function() {
+       $('.databaseCard').collapse('show');
+     }, function() {
+       $('.databaseCard').collapse('hide');
+     }
+);
